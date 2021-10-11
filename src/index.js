56204550d67a96846eccf54c3c049bae792433d7
Punisherl337 <92337248+Punisherl337@users.noreply.github.com>
2021-10-12 00:42:34 +0300

    Add layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('started');
+const express = require('express');
+const handlebars = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+
+//views setup hbs
+app.set('views', path.resolve('./src/views'));
+
+//setup hbs
+app.engine('hbs', handlebars({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs')
+
+
+
+
+app.all('/', (req, res) => {
+    res.render('index', { layout: false });
+});
+
+app.listen(5000, console.log.bind(console, 'Application is running on http://localhost:5000'));
