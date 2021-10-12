4a7b1edff4a3102ca5ad0c62caf098c8ac92558b
Punisherl337 <92337248+Punisherl337@users.noreply.github.com>
2021-10-12 04:06:29 +0300

    Add env variables

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -17,7 +17,7 @@ const createCube = (req, res) => {
 
     cubeService.create(name, description, imageUlr, difficulty);
 
-    res.redirect('/cube/create');
+    res.redirect('/');
 }
 
 const cubeDetails = (req, res) => {
