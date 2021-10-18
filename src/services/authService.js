7a140336ef7b74042e37371f1dbb4d4b7d9821d8
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2021-10-19 02:19:53 +0300

    Verify pw on log-in

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-
+const bcrypt = require("bcrypt");
 
 exports.register = function (username, password, repeatPassowrd) {
   //   return bcrypt.hash(password, 10).then((hash) => {
@@ -7,4 +7,16 @@ exports.register = function (username, password, repeatPassowrd) {
   //   });
 
   return User.create({ username, password });
+};
+
+exports.login = function (username, password) {
+  return User.findByUsername(username)
+    .then((user) => Promise.all([bcrypt.compare(password, user.password), user]))
+    .then(([isValid, user]) => {
+      if (isValid) {
+        return user;
+      } else {
+        throw { message: "Cannot find username or password!" };
+      }
+    });
 };
