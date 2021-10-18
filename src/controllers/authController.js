a78871a69d21c3d7a1813e0d2105e1b299a7645a
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2021-10-18 23:04:46 +0300

    Add User Model

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const authService = require('../services/authService');
 
 router.get('/login', (req, res) =>{
     res.render('auth/login');
@@ -11,5 +12,11 @@ router.post('/login', (req, res) =>{
 router.get('/register', (req, res) => {
     res.render('auth/register');
 });
+
+router.post('/register', (req, res)=> {
+    let{username, password, repeatPassowrd} = req.body;
+    
+    authService.register(username, password, repeatPassowrd);
+})
 
 module.exports = router;
---
+++ src/models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        minlength: [3, 'Username cannot be shorter than 3 characters']
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+});
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
