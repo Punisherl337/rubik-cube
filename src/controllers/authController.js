218ab37bb7d2a60e2534ea71a5b60b33dfa9c0fb
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2021-10-18 22:54:44 +0300

    Add Register Page

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,6 +6,10 @@ router.get('/login', (req, res) =>{
 
 router.post('/login', (req, res) =>{
     res.redirect('/login');
-})
+});
+
+router.get('/register', (req, res) => {
+    res.render('auth/register');
+});
 
 module.exports = router;
