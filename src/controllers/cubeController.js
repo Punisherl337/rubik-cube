b86d48a11462025ac4488e5e71b844967cbc887b
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2021-10-20 02:34:28 +0300

    Delete cube

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -4,7 +4,7 @@ const cubeService = require("../services/cubeService");
 const cubeAccessoryController = require("./cubeAccessoryController");
 const { TOKEN_COOKIE, SECRET } = require("../constants");
 const jwt = require("jsonwebtoken");
-const {isAuth} = require('../middlewares/authMiddleware');
+const { isAuth } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
@@ -32,16 +32,23 @@ const getEditCubePage = (req, res) => {
   res.render("cube/edit");
 };
 
-const getDeleteCubePage = (req, res) => {
-   
-  res.render("cube/delete");
+const getDeleteCubePage = async (req, res) => {
+  let cubeId = req.params.cubeId;
+  let cube = await cubeService.getOne(req.params.cubeId);
+  res.render("cube/delete", cube);
+};
+
+const postDeletePage = async (req, res) => {
+    await cubeService.deleteOne(req.params.cubeId);
+    res.redirect('/');
 };
 
 router.get("/create", getCreateCubePage);
 router.post("/create", isAuth, createCube);
 router.get("/:cubeId", cubeDetails);
-router.get("/:cubeId/edit",isAuth, getEditCubePage);
+router.get("/:cubeId/edit", isAuth, getEditCubePage);
 router.get("/:cubeId/delete", isAuth, getDeleteCubePage);
+router.post('/:cubeId/delete', isAuth,postDeletePage );
 
 router.use("/:cubeId/accessory", cubeAccessoryController);
 
--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -1,51 +1,58 @@
-const Cube = require('../models/Cube');
-const Accessory = require('../models/Accessory')
+const Cube = require("../models/Cube");
+const Accessory = require("../models/Accessory");
 
-const getAll = async() => Cube.find({}).lean();
+const getAll = async () => Cube.find({}).lean();
 
-const getOne = (id) => Cube.findById(id).populate('accessories').lean();
+const getOne = (id) => Cube.findById(id).populate("accessories").lean();
 
 const create = (name, description, imageUrl, difficulty) => {
-    let cube = new Cube({
-        name,
-        description,
-        imageUrl,
-        difficulty
-    });
-    return Cube.create(cube);
-}
-
-const search = async(text, from, to) => {
-    let result = await getAll();
-
-    if (text) {
-        result = result.filter(x => x.name.toLocaleLowerCase().includes(text.toLocaleLowerCase()));
-    }
-
-    if (from) {
-        result = result.filter(x => x.difficulty >= from);
-    }
-
-    if (to) {
-        result = result.filter(x => x.difficulty <= to);
-    }
-
-    return result;
+  let cube = new Cube({
+    name,
+    description,
+    imageUrl,
+    difficulty,
+  });
+  return Cube.create(cube);
 };
 
-const attachAccessory = async(cubeId, accessoryId) => {
-    let cube = await Cube.findById(cubeId);
-    let accessory = await Accessory.findById(accessoryId);
-    cube.accessories.push(accessory);
-    return cube.save();
+const search = async (text, from, to) => {
+  let result = await getAll();
+
+  if (text) {
+    result = result.filter((x) =>
+      x.name.toLocaleLowerCase().includes(text.toLocaleLowerCase())
+    );
+  }
+
+  if (from) {
+    result = result.filter((x) => x.difficulty >= from);
+  }
+
+  if (to) {
+    result = result.filter((x) => x.difficulty <= to);
+  }
+
+  return result;
+};
+
+const attachAccessory = async (cubeId, accessoryId) => {
+  let cube = await Cube.findById(cubeId);
+  let accessory = await Accessory.findById(accessoryId);
+  cube.accessories.push(accessory);
+  return cube.save();
+};
+
+const deleteOne = (cubeId) => {
+  return Cube.findByIdAndDelete(cubeId);
 };
 
 const cubeService = {
-    create,
-    getAll,
-    getOne,
-    search,
-    attachAccessory
-}
+  create,
+  getAll,
+  getOne,
+  search,
+  attachAccessory,
+  deleteOne,
+};
 
 module.exports = cubeService;
